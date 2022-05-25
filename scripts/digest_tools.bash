31835ec1f2f4a4abc3a87a7d2488f20c8cfca28e
Sam Salisbury <samsalisbury@gmail.com>
2022-05-25 16:45:45 +0100

    fix grep usage

--- scripts/digest_tools.bash
+++ scripts/digest_tools.bash
@@ -36,7 +36,7 @@ digest_names() {
 
 digest_path_rel() {
 	local DIGEST_NAME="$1"
-	digest_names | grep -q "^$DIGEST_NAME\$" || {
+	digest_names | grep -Eq "^$DIGEST_NAME\$" || {
 		die "Digest name '$DIGEST_NAME' not recognised; must be one of: $(digest_names | xargs)"
 	}
 	echo "$META_DIR/${DIGEST_NAME}_digest"	
