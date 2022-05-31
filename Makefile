2c70a8caac502bd53124a315dc7360ec5beb1384
Sam Salisbury <samsalisbury@gmail.com>
2022-05-31 11:18:14 +0100

    add local 'example' target
    - This allows running primary build, verification build, and
      compare digests all together locally

--- Makefile
+++ Makefile
@@ -25,3 +25,24 @@ LDFLAGS += -X 'main.RevisionTime=2022-05-30T14:45:00+00:00'
 .PHONY: example-app
 example-app:
 	@cd testdata/example-app && go build -ldflags "$(LDFLAGS)" . && ./example-app
+
+example: export INSTRUCTIONS := cd testdata/example-app && go build -trimpath -ldflags "$(LDFLAGS)" -o "$$BIN_PATH"
+example: export OS                  := $(shell go env GOOS)
+example: export ARCH                := $(shell go env GOARCH)
+example: export PRODUCT_VERSION     := 1.2.3
+example: export PACKAGE_NAME        := example-app
+example: export EXAMPLE_TMP         := $(shell mktemp -d)
+example: export GITHUB_ENV          := $(EXAMPLE_TMP)/github_env
+example: export GITHUB_STEP_SUMMARY := $(EXAMPLE_TMP)/github_step_summary
+example: export PRIMARY_BUILD_ROOT  := $(EXAMPLE_TMP)/primary
+example: export VERIFICATION_BUILD_ROOT  := $(EXAMPLE_TMP)/verification
+example:
+	rm -rf "$(EXAMPLE_TMP)" && mkdir -p "$(EXAMPLE_TMP)"
+	cp -rf . "$(PRIMARY_BUILD_ROOT)"
+	cd $(PRIMARY_BUILD_ROOT) && \
+		source scripts/inputs.bash && \
+		digest_inputs && \
+		./scripts/primary_build && \
+		./scripts/local_verification_build && \
+		trap 'cat $(GITHUB_STEP_SUMMARY)' EXIT && \
+		./scripts/compare_digests
---
+++ scripts/inputs.bash
@@ -0,0 +1,132 @@
+# Bash library
+
+# shellcheck source=scripts/standard_header.bash
+source "${BASH_SOURCE%/*}/standard_header.bash"
+
+# This function exports environment variables to GITHUB_ENV
+# so they can be used by future steps in the GitHub Actions job.
+#
+# It either calls 'forward_env' or 'set_env' for each one.
+#
+#   forward_env: Forwards the already-set value if it's already set
+#                or sets it to a default value if provided
+#                or results in an error if there's no value or
+#   set_env:     Always sets the env to the specified value.
+#
+digest_inputs() {
+
+	# Pass through env vars from required action inputs.
+
+	forward_env INSTRUCTIONS
+	forward_env OS
+	forward_env ARCH
+	forward_env PRODUCT_VERSION
+	forward_env PACKAGE_NAME
+
+	# Pass through env vars from optional action inputs (or set to default value).
+
+	forward_env BIN_NAME "$(remove_enterprise_suffix "$PACKAGE_NAME")"
+	forward_env ZIP_NAME "${PACKAGE_NAME}_${PRODUCT_VERSION}_${OS}_${ARCH}.zip"
+	
+	# Set relative paths used to store various build artifacts.
+	
+	set_env TARGET_DIR "dist"
+	set_env ZIP_DIR    "out"
+	set_env META_DIR   ".meta"
+	set_env BIN_PATH   "$TARGET_DIR/$BIN_NAME"
+	set_env ZIP_PATH   "$ZIP_DIR/$ZIP_NAME"
+
+	# Set absolute paths for the primary and verification builds and artifacts.
+
+	forward_env PRIMARY_BUILD_ROOT "$PWD"
+	set_env     BIN_PATH_PRIMARY   "$PRIMARY_BUILD_ROOT/$BIN_PATH"
+	set_env     ZIP_PATH_PRIMARY   "$PRIMARY_BUILD_ROOT/$ZIP_PATH"
+
+	forward_env VERIFICATION_BUILD_ROOT "$(adjacent_path "$PWD" "verification")"
+	set_env     BIN_PATH_VERIFICATION   "$VERIFICATION_BUILD_ROOT/$BIN_PATH"
+	set_env     ZIP_PATH_VERIFICATION   "$VERIFICATION_BUILD_ROOT/$ZIP_PATH"
+	
+	# Gather contextual info from git.
+
+	set_env PRODUCT_REVISION      "$(git rev-parse HEAD)"
+	set_env PRODUCT_REVISION_TIME "$(commit_time_utc "$PRODUCT_REVISION")"
+
+	# Set Go-specific vars.
+
+	set_env GOOS "$OS"
+	set_env GOARCH "$ARCH"
+}
+
+adjacent_path() { echo "$(dirname "$1")/$2"; }
+
+remove_enterprise_suffix() {
+	echo "${1%-enterprise}"
+}
+
+commit_time_utc() {
+	local COMMIT_ID="$1"
+	local T
+	T="$(git show -s --format=%cI "$COMMIT_ID")"
+	date --utc --iso-8601=seconds -d "$T"
+}
+
+
+export_to_github_job() { local NAME="$1"
+	{
+		echo "$NAME<<EOF"
+		echo "${!NAME}"
+		echo "EOF"
+	} >> "$GITHUB_ENV"
+	# For testing purposes we also write to a standard
+	# script file we can source in the tests to see which
+	# variables have been exported with which values.
+	echo "export $NAME='${!NAME}'" >> "$GITHUB_ENV.export"
+	log "Exported to GITHUB_ENV: $NAME='${!NAME}'"
+}
+
+# forward_env passes the current value of the named env var
+# through to GitHub, or uses the default value if that variable
+# is currently empty. If both are empty, it's an error.
+forward_env() {
+	local NAME="$1"
+	local DEFAULT="${2:-}"
+	export_env_or_default "$NAME" "$DEFAULT"
+	export_to_github_job "$NAME"
+}
+
+# set_env sets an env var and preserves it for the 
+set_env() {
+	local NAME="$1"
+	local VALUE="$2"
+	export "$NAME"="$VALUE"
+	export_env "$NAME"
+	export_to_github_job "$NAME"
+}
+
+# export_env_or_default exports an env var with the name specified,
+# if that variable is already nonempty, then its original value is
+# preserved. If it is unset or empty, then it is set to the default
+# value.
+export_env_or_default() {
+	local NAME="$1"
+	local DEFAULT="${2:-}"
+	# Already got a value? Just export it as that.
+	try_export_nonempty "$NAME" && return
+	# Default value provided? Export it with that value.
+	test -n "$DEFAULT" && {
+		export "$NAME"="$DEFAULT"
+		return
+	}
+	err "Attempting to export an empty or unset env var with no default value."
+}
+
+export_env() {
+	local NAME="$1"
+	try_export_nonempty "$NAME" || err "Attempting to export an empty or unset env var."
+}
+
+try_export_nonempty() {
+	local NAME="$1"
+	test -n "${!NAME:-}" || return 1
+	export "$NAME"="${!NAME}"
+}
