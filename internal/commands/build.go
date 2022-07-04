99e10b8206d8b2713512d5b17b37fcd72b046da0
Sam Salisbury <samsalisbury@gmail.com>
2022-07-04 12:34:26 +0100

    add build run and env subcommands

--- internal/commands/build.go
+++ internal/commands/build.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"flag"
+
 	"github.com/hashicorp/actions-go-build/internal/config"
 	"github.com/hashicorp/actions-go-build/pkg/action"
 	"github.com/hashicorp/actions-go-build/pkg/build"
@@ -8,12 +10,63 @@ import (
 
 func Build() action.Command {
 	return action.Command{
+		Subcommands: action.Subcommands{
+			"run": BuildRun,
+			"env": BuildEnv,
+		},
+	}
+}
+
+type buildCommandConfig struct {
+	verification bool
+}
+
+func (bcc *buildCommandConfig) flagSet() *flag.FlagSet {
+	fs := flag.NewFlagSet("buildrun", flag.ContinueOnError)
+	fs.BoolVar(&bcc.verification, "verification", false, "verification build")
+	return fs
+}
+
+func (bcc *buildCommandConfig) buildConfig() (config.BuildConfig, error) {
+	c, err := config.FromEnvironment()
+	if err != nil {
+		return config.BuildConfig{}, err
+	}
+	if bcc.verification {
+		return c.VerificationBuildConfig()
+	}
+	return c.PrimaryBuildConfig()
+}
+
+func BuildRun() action.Command {
+	var bcc buildCommandConfig
+	return action.Command{
+		Flags: bcc.flagSet,
 		Run: func(args []string) error {
-			b, err := build.New(config.BuildConfig{})
+			c, err := bcc.buildConfig()
+			if err != nil {
+				return err
+			}
+			b, err := build.New(c)
 			if err != nil {
 				return err
 			}
 			return b.Run()
+		},
+	}
+}
+
+func BuildEnv() action.Command {
+	var bcc buildCommandConfig
+	return action.Command{
+		Run: func(args []string) error {
+			c, err := bcc.buildConfig()
+			if err != nil {
+				return err
+			}
+			print(c)
+			// TODO: Print build env.
+			return nil
 		},
 	}
 }
