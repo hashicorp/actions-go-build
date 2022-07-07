6ce0c5623b4298a16416b83de16e8a8fae890565
Sam Salisbury <samsalisbury@gmail.com>
2022-07-07 17:15:39 +0100

    add logging for verification build

--- pkg/commands/verification.go
+++ pkg/commands/verification.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"log"
+
 	"github.com/hashicorp/actions-go-build/internal/config"
 	"github.com/hashicorp/actions-go-build/pkg/cli"
 	"github.com/hashicorp/actions-go-build/pkg/crt"
@@ -11,9 +13,11 @@ import (
 // directory to the verification build root.
 var Verification = cli.LeafCommand("verification", "run the verification build", func(cli.None) error {
 	return runBuildWithConfig(func(c config.Config) (crt.BuildConfig, error) {
+		log.Printf("Copying %s to %s", c.PrimaryBuildRoot, c.VerificationBuildRoot)
 		if err := cp.Copy(c.PrimaryBuildRoot, c.VerificationBuildRoot); err != nil {
 			return crt.BuildConfig{}, err
 		}
+		log.Printf("Running verification build")
 		return c.VerificationBuildConfig()
 	})
 })
