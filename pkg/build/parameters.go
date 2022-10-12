48c5a85cecbcf8514885e67081f87b7a573b53c3
Sam Salisbury <samsalisbury@gmail.com>
2022-10-12 15:13:15 +0100

    detect installed go version correctly
    - Don't assume it's the same as the version that built
      actions-go-build itself.

--- pkg/build/parameters.go
+++ pkg/build/parameters.go
@@ -2,6 +2,7 @@ package build
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
 	"strings"
 
@@ -34,9 +35,20 @@ func (bp Parameters) trimSpace() Parameters {
 	return bp
 }
 
+func getInstalledGoVersion() (string, error) {
+	got, err := exec.Command("go", "env", "GOVERSION").Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimPrefix(string(got[:len(got)-1]), "go"), nil
+}
+
 func (bp Parameters) setDefaults(p crt.Product) (Parameters, error) {
 	if bp.GoVersion == "" {
-		bp.GoVersion = strings.TrimPrefix(runtime.Version(), "go")
+		var err error
+		if bp.GoVersion, err = getInstalledGoVersion(); err != nil {
+			return bp, err
+		}
 	}
 	if bp.OS == "" {
 		bp.OS = runtime.GOOS
